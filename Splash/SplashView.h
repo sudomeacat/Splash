bdb8750b12007bd8f777477c38c33dd050d97a39
krishkalai <krishkalai07@gmail.com>
2018-04-28 19:45:49 -0700

    Initial Commit

---
+++ Splash/SplashView.h
@@ -0,0 +1,13 @@
+//
+//  SplashView.h
+//  Splash
+//
+//  Created by Krish Kalai on 4/28/18.
+//  Copyright © 2018 Krish Kalai. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SplashView : ScreenSaverView
+
+@end
---
+++ Splash/SplashView.m
@@ -0,0 +1,52 @@
+//
+//  SplashView.m
+//  Splash
+//
+//  Created by Krish Kalai on 4/28/18.
+//  Copyright © 2018 Krish Kalai. All rights reserved.
+//
+
+#import "SplashView.h"
+
+@implementation SplashView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
